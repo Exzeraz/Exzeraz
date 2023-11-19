2a542c572aa8d9178461989cbc3a124f085cc65b
Exzeraz <100132220+Exzeraz@users.noreply.github.com>
2023-11-19 16:51:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOPPING</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="navbar/navbar.css">
+</head>
+<body>
+    <?php 
+        include "navbar/navbar.html";
+    ?>
+</body>
+</html>
