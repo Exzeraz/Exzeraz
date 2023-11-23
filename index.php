3e903134625aa3825462e091448fbfba48ad10cf
Exzeraz <100132220+Exzeraz@users.noreply.github.com>
2023-11-23 21:35:05 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,52 @@
+<?php 
+    session_start()
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHOPPING</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="navbar/navbar.css">
+    <title>Home</title>
+    <?php 
+        include "link.html";
+    ?>
 </head>
 <body>
     <?php 
-        include "navbar/navbar.html";
-    ?>
+        include "navbar/navbar.php";
+    ?><br><br><br>
+    <div class="container">
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+        <span class="item"></span>
+
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -6,15 +6,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project01</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/form.css">
-    <link rel="stylesheet" href="navbar/navbar.css">
-
+    <title>Login</title>
+    <?php 
+        include "link.html";
+    ?>
 </head>
 <body>
     <?php 
-        include "navbar/navbar.html";
+        include "navbar/navbar.php";
     ?>
 
     <div class="form-container">
